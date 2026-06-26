873e6116048c76db9b0779de9306ed346e5b228e
diegofmk92 <diegofmk92@gmail.com>
2026-06-26 20:26:27 +0200

    chore: remove dead AudioIOSpike package (abandoned BlackHole capture spike)

--- Packages/AudioIOSpike/Sources/AudioIOSpikeSupport/AudioIOSpikeRunner.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum AudioIOSpikeRunner {
-    public static func run(captureDurationSeconds: TimeInterval = 1.0) throws -> AudioIOSpikeReport {
-        let partA = try SyntheticValidationRunner.run()
-        let partB = LiveInputSpikeRunner.run(captureDurationSeconds: captureDurationSeconds)
-        return AudioIOSpikeReport(partA: partA, partB: partB)
-    }
-}
